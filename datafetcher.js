55278844a0b005470b58c6b94d3bc09eee0cf5ac
Cameron <cameronvivio@gmail.com>
2025-06-11 15:04:47 -0700

    Added a test URL for New Yorks crime data base

--- datafetcher.js
+++ datafetcher.js
@@ -1,15 +1,36 @@
-const fastify = require('fastify')({ logger: true })
+// Require the framework and instantiate it
 
+// ESM
+//import Fastify from 'fastify'
+
+// CommonJs
+const fastify = require('fastify')({
+  logger: true
+})
+
+const NewYork_URL = 'https://data.cityofnewyork.us/resource/qgea-i56i.json'
 // Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.get('/crimes', async (request, reply) => {
+    try{
+        const response = await fetch(NewYork_URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data)
+        return data
+    }catch (error){
+        console.log(error.message)
+    }
 })
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
-  if (err) {
+ const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  fastify.log.info(`server listening on ${address}`)
-})
+}
+start()
