dc217a7018c762c7d31b5ccd842a4d2dc82034f1
Cam <cameronvivio@gmail.com>
2025-07-02 18:08:57 -0700

    Trying to get multiple endpoints to work

--- datafetcher.js
+++ datafetcher.js
@@ -1,7 +1,6 @@
 // Require the framework and instantiate it
 // ESM
 //import Fastify from 'fastify'
-
 // CommonJs
 const fastify = require('fastify')({
   logger: true
@@ -9,11 +8,10 @@ const fastify = require('fastify')({
 fastify.register(require('@fastify/cors'), {
   origin: '*'  // This allows all origins - be more restrictive in production
 })
-
 let dateStored = "2024-01-01";
 //EX: https://data.cityofnewyork.us/resource/qgea-i56i.json?cmplnt_fr_dt=2024-01-01
 const NewYork_URL = `https://data.cityofnewyork.us/resource/qgea-i56i.json?cmplnt_fr_dt=${dateStored}`
-
+const Seattle_URL = `https://data.seattle.gov/resource/tazs-3rd5.json?$where=offense_date between '${dateStored}T00:00:00.000' and '${dateStored}T23:59:59.999'`
 fastify.post('/date', async (request, reply) => {
     try{
         console.log(request.body)
@@ -25,24 +23,37 @@ fastify.post('/date', async (request, reply) => {
         console.log(error)
     }
 })
-console.log(dateStored, NewYork_URL)
+//console.log(dateStored, NewYork_URL)
 // Declare a route
 fastify.get('/crimes', async (request, reply) => {
     try{
         const NewYork_URL = `https://data.cityofnewyork.us/resource/qgea-i56i.json?cmplnt_fr_dt=${dateStored}`
-        console.log(NewYork_URL)
-        const response = await fetch(NewYork_URL);
-        if (!response.ok) {
+        const Seattle_URL = `https://data.seattle.gov/resource/tazs-3rd5.json?$where=offense_date between '${dateStored}T00:00:00.000' and '${dateStored}T23:59:59.999'`
+
+	const response = await fetch(NewYork_URL);
+	const response1 = await fetch(Seattle_URL);
+        
+	    if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data)
-        const filteredData = data.map(crime => ({
+        const data1 = await response1.json();
+	
+	const filteredData = data.map(crime => ({
             date: crime.cmplnt_fr_dt,
             crimeType: crime.ofns_desc,
             longitude: crime.longitude,
             latitude: crime.latitude
         }));
+const filteredData1 = data1.map(crime => ({
+    	  date: crime.offense_date,
+    	  crimeType: crime.offense_sub_category,
+    	  longitude: crime.longitude,
+    	  latitude: crime.latitude
+	}));
+
+	filteredData.push(filteredData1)
+	console.log(filteredData1)
         return filteredData
     }catch (error){
         console.log(error.message)
